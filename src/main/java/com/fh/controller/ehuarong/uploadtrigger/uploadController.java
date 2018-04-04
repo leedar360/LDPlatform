620f439571585d89c4ffff65412786a243c30a34
Evan <50402727@qq.com>
2018-04-04 15:29:26 +0800

    init project

---
+++ src/main/java/com/fh/controller/ehuarong/uploadtrigger/uploadController.java
@@ -0,0 +1,37 @@
+package com.fh.controller.ehuarong.uploadtrigger;
+
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Resource;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import com.fh.controller.base.BaseController;
+import com.fh.entity.Page;
+import com.fh.util.AppUtil;
+import com.fh.util.ObjectExcelView;
+import com.fh.util.PageData;
+import com.fh.util.Jurisdiction;
+import com.fh.util.Tools;
+import com.fh.service.ehuarong.orderinfo.OrderinfoManager;
+
+/**
+ * 说明：上传excel工具
+ * 创建人：wp
+ * 创建时间：2018-03-29
+ */
+@Controller
+@RequestMapping(value="/uploadtrigger")
+public class uploadController extends BaseController {
+
+}
